c1faf48797e1b57eac793a4761dfd2aee99b9af1
Ervic Padilla <valid.replica@gmail.com>
2023-02-25 15:57:59 +0800

    Add game logic

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 "use strict";
 
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(randomNumber);
+
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No input!";
+  } else if (guess === randomNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector(".number").textContent = randomNumber;
+    document.querySelector(".highscore").textContent = score;
+  } else if (guess > randomNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+    }
+  } else if (guess < randomNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
