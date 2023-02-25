ebe514d1f932fc02ecafb195dbf52c8196b9da66
Ervic Padilla <valid.replica@gmail.com>
2023-02-25 16:08:52 +0800

    Add style when you win and when you lose

--- script.js
+++ script.js
@@ -11,25 +11,41 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No input!";
-  } else if (guess === randomNumber) {
+  }
+
+  // When player wins
+  else if (guess === randomNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = randomNumber;
     document.querySelector(".highscore").textContent = score;
-  } else if (guess > randomNumber) {
+  }
+
+  // When guess is too high
+  else if (guess > randomNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high!";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector("body").style.backgroundColor = "#FF6962";
+      document.querySelector(".score").textContent = 0;
     }
-  } else if (guess < randomNumber) {
+  }
+
+  // When guess is too low
+  else if (guess < randomNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too low!";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector("body").style.backgroundColor = "#FF6962";
+
       document.querySelector(".score").textContent = 0;
     }
   }
